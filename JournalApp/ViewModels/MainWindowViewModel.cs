e1a66d342b2a1af016e5eda85234bb5a2660e641
SaZyGH <103922719+SaZyGH@users.noreply.github.com>
2023-03-28 15:37:11 +0300

    Добавьте файлы проекта.

---
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JournalApp.ViewModels.Base;
+
+namespace JournalApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string title = "Привозгрин где?";
+
+        public string Title
+        {
+            get { return title; } 
+            set { Set(ref title, value); } 
+        }
+    }
+}
